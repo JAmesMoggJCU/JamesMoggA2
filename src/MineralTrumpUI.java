0c5eff5dd574d537d2ff1c9fe5faae749ecc93f2
JAmesMoggJCU <james.mogg@my.jcu.edu.au>
2016-10-23 13:30:02 +1000

    Completed the game play UI

--- src/MineralTrumpUI.java
+++ src/MineralTrumpUI.java
@@ -1,77 +1,69 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-
+import java.util.ArrayList;
 
 /**
  * Created by moggj_000 on 12/10/2016.
  */
-public class MineralTrumpUI implements ItemListener {
-    JPanel cards; //panel with CardLayout
-    final static String BUTTONPANEL = "Cards with JButtons";
+public class MineralTrumpUI extends JFrame implements ActionListener {
+    static Game instance;
+    static int playerLoop = 0;
+    BorderLayout borderLayout = new BorderLayout();
+    JLabel playerName = new JLabel("The current player is: " + instance.playersArray.get(playerLoop).Name);
+    JLabel statusMessage = new JLabel("The type for the current round is: " + instance.roundType);
+    JPanel contentPane = new JPanel();
+    public ArrayList<JButton> buttons = new ArrayList<JButton>();
+    JButton passButton = new JButton("Pass");
 
-    public void addComponentToPane(Container pane){
-        JPanel comboBoxPane = new JPanel();//FlowLayout
-        String comboBoxItems[] = {BUTTONPANEL};
-        JComboBox cb = new JComboBox(comboBoxItems);
-        cb.setEditable(false);
-        cb.addItemListener(this);
-        comboBoxPane.add(cb);
-
-        //Create cards
-        JPanel card1 = new JPanel();
-        card1.add(new JButton("Button 1"));
-        card1.add(new JButton("Button 2"));
-        card1.add(new JButton("Button 3"));
-
-        //panel that conatian the cards
-        cards = new JPanel(new CardLayout());
-        cards.add(card1, BUTTONPANEL);
-
-        pane.add(comboBoxPane, BorderLayout.PAGE_START);
-        pane.add(cards, BorderLayout.CENTER);
-    }
-    public void itemStateChanged(ItemEvent evt){
-        CardLayout cl = (CardLayout)(cards.getLayout());
-        cl.show(cards, (String)evt.getItem());
-    }
-    private static void ShowGUI(){
-        // set up the Window
-        JFrame frame = new JFrame("Mineral Super Trumps");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //Content pane
-        MineralTrumpUI MinTrump = new MineralTrumpUI();
-        MinTrump.addComponentToPane(frame.getContentPane());
-
-        //Display Window
-        frame.pack();
-        frame.setVisible(true);
-
-    }
-    public static void main(String[] args){
-        try {
-            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
-            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
-        } catch (UnsupportedLookAndFeelException ex) {
-            ex.printStackTrace();
-        } catch (IllegalAccessException ex) {
-            ex.printStackTrace();
-        } catch (InstantiationException ex) {
-            ex.printStackTrace();
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
+    public void getCardButtons() {
+        for (int x = 0; x < 10; x++) {
+            buttons.add(new JButton(" " + x));
+            buttons.get(x).addActionListener(this);
+            contentPane.add(buttons.get(x));
         }
-        /* Turn off metal's use of bold fonts */
-        UIManager.put("swing.boldMetal", Boolean.FALSE);
+    }
+    public void gameOverCheck() {
+        if (instance.playersArray.size() == 1) {
+            JOptionPane.showMessageDialog(null, "The game is now over");
+            JOptionPane.showMessageDialog(null, "Congratulations Winners: ");
+            dispose();
+        }
+    }
+    public void emptyHandCheck() {
+        if (instance.playersArray.get(playerLoop).PlayerHand.size() == 0) {
 
-        //Schedule a job for the event dispatch thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                ShowGUI();
-            }
-        });
+        }
+    }
+    public void playerLoopReset() {
+        if (playerLoop == instance.playersArray.size() - 1) {
+            playerLoop = -1;
+        }
+    }
+    public MineralTrumpUI(Game gameInstance) {
+        super("Mineral Super Trumps");
+        setLayout(borderLayout);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        add(playerName, borderLayout.NORTH);
+        add(statusMessage, borderLayout.SOUTH);
+        add(contentPane, borderLayout.CENTER);
+        instance = gameInstance;
+        JOptionPane.showMessageDialog(null, "It is npw " + instance.playersArray.get(playerLoop).Name + "'s tuen");
+        getCardButtons();
+        contentPane.add(passButton);
+        passButton.addActionListener(this);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (!((JButton) e.getSource()).getText().contains("Pass")) {
+            System.out.println("you clicked a card");
+        } else if (e.getSource() == passButton) {
+            playerLoopReset();
+            statusMessage.setText("You have chosen to pass");
+            ++playerLoop;
+            JOptionPane.showMessageDialog(null, "It is now " + instance.playersArray.get(playerLoop).Name + "'s turn");
+            playerName.setText("The current player is: " + instance.playersArray.get(playerLoop).Name);
+        }
     }
 }
 
